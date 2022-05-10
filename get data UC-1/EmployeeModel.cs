c48d063c730431d8f2ef74b0632e6a967354adb9
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-10 23:10:22 +0530

    adding property of Employee

---
+++ get data UC-1/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Demo
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public long PhoneNumber { get; set; }
+        public double BasicPay { get; set; }
+        public string Address { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public double TaxablePay { get; set; }
+        public double NetPay { get; set; }
+        public double IncomeTax { get; set; }
+        public double Deduction { get; set; }
+    }
+}
