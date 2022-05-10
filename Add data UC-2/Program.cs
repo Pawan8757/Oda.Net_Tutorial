6f109733573f26b02be6a29a411d40a456691909
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-10 23:15:14 +0530

    adding main program

---
+++ Add data UC-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ADO.NET_Demo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the tutorial of ADO.NET Tutorial");
+            Console.WriteLine("\n \n");
+            EmployeeModel emp=new EmployeeModel();
+            emp.EmployeeName = "Pawan";
+            emp.EmployeeID = 1;
+            emp.Address = "Rafiganj Aurangabad";
+            emp.PhoneNumber = 9304314938;
+            emp.BasicPay = 700000;
+            emp.Gender = "M";
+            
+
+            EmployeeRepo empRepo = new EmployeeRepo();
+            empRepo.GetAllEmployees();
+            empRepo.AddEmployee(emp);
+           
+            Console.ReadLine();
+        }
+    }
+}
